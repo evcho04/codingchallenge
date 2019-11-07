6cbfe04767363ca5bc0134fa34df54c099d011bd
evcho04 <47353712+evcho04@users.noreply.github.com>
2019-11-07 18:51:35 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-for(let i=99; i>0;i--){
+for(let i=99; i>-1;i--){
   const beer = 99;
   let less = i-1;
   const count=+1;
@@ -8,6 +8,5 @@ else if(i===49)console.log (i+' bottlesh of beer on the wall, ' + i + ' bottlesh
 else if (i > 2) console.log(i+' bottlesh of beer on the wall, ' + i + ' bottles of beer.\nTake one down and pass it around. ' + less + ' bottlesh of beer on the wall' );
 else if (i>1) console.log (i+ ' bottlelesh of beer on the wall, ' + i + ' bottlesh of beer.\nTake one down and pass it around, ' +less+ ' bottle of beer on the wall.');
 else if (i===1)console.log(i+' bottle of beer on the wall, ' + i + ' bottle of beer.\nTake one down and pass it around, no more bottlesh of beer on the wall.');
-else (i); console.log('No more bottlesh of beer on the wall,no more bottlesh of beer.Go to the store and buy some more, ' + beer + ' bottlesh of beer on the wall');
-
+else   console.log('No more bottlesh of beer on the wall,no more bottlesh of beer.Go to the store and buy some more, ' + beer + ' bottlesh of beer on the wall');
 }
