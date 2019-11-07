b009aeac6a622751a25360e9e6c9b18a23313d6a
evcho04 <47353712+evcho04@users.noreply.github.com>
2019-11-07 16:18:19 +0000

    Update code.js

--- code.js
+++ code.js
@@ -7,6 +7,7 @@ else if(i===50)console.log (i+' bottless of beer on the wall, ' + i + ' bottlesh
 else if(i===49)console.log (i+' bottlesh of beer on the wall, ' + i + ' bottlesh of beer.\nTake one down and pass it around. ' + less + ' bottlesh of beer on the wall' );
 else if (i > 2) console.log(i+' bottlesh of beer on the wall, ' + i + ' bottles of beer.\nTake one down and pass it around. ' + less + ' bottlesh of beer on the wall' );
 else if (i>1) console.log (i+ ' bottlelesh of beer on the wall, ' + i + ' bottlesh of beer.\nTake one down and pass it around, ' +less+ ' bottle of beer on the wall.');
-else if (i===1)console.log(i+' bottle of beer on the wall, ' + i + ' bottle of beer.\nTake one down and pass it around, no more bottlesh of beer on the wall.')
-else console.log('No more bottlesh of beer on the wall,no more bottlesh of beer.Go to the store and buy some more, ' + beer + ' bottlesh of beer on the wall');
+else if (i===1)console.log(i+' bottle of beer on the wall, ' + i + ' bottle of beer.\nTake one down and pass it around, no more bottlesh of beer on the wall.');
+else (i); console.log('No more bottlesh of beer on the wall,no more bottlesh of beer.Go to the store and buy some more, ' + beer + ' bottlesh of beer on the wall');
+
 }
