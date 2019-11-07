7fd690ebd3a4d3615088ad15f3e9f6cea6b94c33
evcho04 <47353712+evcho04@users.noreply.github.com>
2019-11-07 14:58:51 +0000

    Adding files

---
+++ code.js
@@ -0,0 +1,8 @@
+for(let i=99;i>0;i--){
+  const beer = 99;
+  let less = i-1;
+if (i<100) console.log(i+'bottles of beer on the wall,' + ' ' + i + 'bottle of beer.' + 'Take one down and pass it around, ' + less + ' bottlesh of beer on the wall.' )
+else console.log
+const count=+1;
+  console.log('bottles of beer on the wall'+ beer);
+}
